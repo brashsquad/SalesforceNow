db4ca3946557e858aaebb5f4f8cd86c7cbe9bccc
baolongnt <baolongnt@gmail.com>
2013-11-18 21:03:16 -0800

    . SearchActivity minor UI cleanup   . No action bar   . Remove from stack

--- src/main/java/com/blntsoft/salesforcenow/SearchActivity.java
+++ src/main/java/com/blntsoft/salesforcenow/SearchActivity.java
@@ -23,6 +23,8 @@ public class SearchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getActionBar().hide();
+
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speak_now_hint));
@@ -50,6 +52,8 @@ public class SearchActivity extends Activity {
             Intent searchIntent = new Intent(this, SearchResultActivity.class);
             searchIntent.putExtra(SearchResultActivity.SEARCH_STRING_EXTRA, matches.get(0));
             startActivity(searchIntent);
+
+            finish();
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
