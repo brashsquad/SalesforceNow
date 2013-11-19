fe2a3c39a33a2feb3baad6c94127698d1ce5a6cf
Arnaud Delabarre <delabarre.arnaud@gmail.com>
2013-11-19 00:55:01 -0800

    Added SOSL search

--- src/main/java/com/blntsoft/salesforcenow/SearchResultActivity.java
+++ src/main/java/com/blntsoft/salesforcenow/SearchResultActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.blntsoft.salesforcenow.service.SpeechRecognizerService;
 import com.salesforce.androidsdk.rest.RestClient;
@@ -11,14 +13,26 @@ import com.salesforce.androidsdk.rest.RestRequest;
 import com.salesforce.androidsdk.rest.RestResponse;
 import com.salesforce.androidsdk.ui.sfnative.SalesforceActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by baolongnt on 11/18/13.
  */
 public class SearchResultActivity extends SalesforceActivity {
 
-    public static String SEARCH_STRING_EXTRA        = "Search String";
+    public static String SEARCH_STRING_EXTRA    = "Search String";
+    private static final int LIMIT = 20;
+
+    private static final String TAG = "SearchResultActivity";
 
     private RestClient client;
     private View rootView;
@@ -51,29 +65,78 @@ public class SearchResultActivity extends SalesforceActivity {
         try {
             Intent i = getIntent();
             String searchString = i.getStringExtra(SEARCH_STRING_EXTRA);
+
             if (searchString != null
                         && !searchString.equals("")) {
-                RestRequest request = RestRequest.getRequestForSearch("v29.0", searchString);
+                String sosl = getSOSL(searchString);
+
+                Log.d(TAG, "SOSL: " + sosl);
+
+                final Map<String, String> nameById = new HashMap<String, String>();
+                RestRequest request = RestRequest.getRequestForSearch("v29.0", sosl);
                 client.sendAsync(request, new RestClient.AsyncRequestCallback() {
+
                     @Override
                     public void onSuccess(RestRequest request, RestResponse response) {
+                        try {
 
+                            JSONArray result = response.asJSONArray();
+                            Log.d(TAG, "Count Result: " + result.length());
+                            ArrayList<String> resultList = new ArrayList<String>();
+                            if (result.length() > 0) {
+                                for (int i = 0; i < result.length(); i++) {
+                                    JSONObject object = result.getJSONObject(i);
+                                    String id = object.getString("Id");
+                                    String name = object.getString("Name");
+                                    Log.d(TAG, "Id: " + id);
+                                    Log.d(TAG, "Name: " + name);
+                                    resultList.add(name);
+                                    /*JSONObject a = object.geeById.put(idtJSONObject("attributes");
+                                    for (Iterator j = a.keys(); j.hasNext();) {
+                                        String key = j.next().toString();
+                                        String value = object.getString(key);
+                                        Log.d(TAG, key + " : " + value);
+                                    }*/
+                                }
+                            } else resultList.add("No result found !");
+
+                            ListView l = (ListView) findViewById(R.id.listView);
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(SearchResultActivity.this, android.R.layout.simple_list_item_1, resultList.toArray(new String[0]));
+                            l.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            Log.e(TAG, null, e);
+                        } catch (IOException e) {
+                            Log.e(TAG, null, e);
+                        }
                     }
 
                     @Override
                     public void onError(Exception exception) {
-
+                        Log.d(TAG, "Error");
                     }
                 });
+
+                if (nameById.size() > 0) {
+                    Log.d(TAG, "MAP has " + nameById.size());
+                } else Log.d(TAG, "MAP has " + nameById.size());
             }
             else {
-
+                //NOTHING TO SEARCH
             }
+
         }
         catch (IOException e) {
             Log.e(SalesforceNowApp.LOG_TAG, null, e);
         }
 
+    }
+
+    private String getSOSL(String searchString) {
+        return "FIND {*" + searchString + "*} " +
+               "IN ALL FIELDS " +
+               "RETURNING Account (id,name) " +
+               "LIMIT " + LIMIT;
     }
 
 
